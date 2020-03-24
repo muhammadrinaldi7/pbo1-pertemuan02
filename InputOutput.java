b3b39b4c2fcba6708c2247ede6a0e35e778306a3
muhammadrinaldi7 <muhammad.rinaldi007.mr@gmail.com>
2020-03-24 09:53:50 +0800

    Modifikasi File Input Output

--- InputOutput.java
+++ InputOutput.java
@@ -4,8 +4,19 @@ public class InputOutput{
  public static void main(String []args){
   Scanner scanner = new Scanner(System.in);
   String nama;
+  int p,l;
   System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
   System.out.println("Hai, "+nama);
+  System.out.println("== Menghitung Luas Persegi Panjang ==");
+  
+  System.out.print("Masukkan Panjang : ");
+  p = scanner.nextInt();
+ 
+  System.out.print("Masukkan Lebar : ");
+  l = scanner.nextInt();
+  
+  int luas = p * l;
+  System.out.print("Luas Persegi Panjang = "+luas);
 }
 }
